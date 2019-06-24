ec0d769e0ad4040e95c3e506ccd7b0bdc69a3a0e
angel <nopaixx@gmail.com>
2019-06-25 00:02:15 +0200

    initial code moved to satellite

---
+++ runcode.py
@@ -0,0 +1,215 @@
+import os
+import json
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import traceback
+import time
+import sys
+
+def c_run_str_code(inputs, str_code):
+#    return 'RUNED'
+    global ret
+    ret = []
+    print("START RUNNING CLIENT CODE")
+    LOC = str_code
+    print("END RUN CLIENT CODE")    
+    exec(LOC)
+
+    return ret
+
+
+class InputPort():
+
+    def __init__(self, name, numport, parentBox):
+        self.name = name
+        self.numport = numport
+        self.parentBox = parentBox
+
+
+class BoxCode():
+
+    def __init__(self, str_code, box_id, n_inputs, n_outputs, json):
+        self.str_code = str_code
+        self.n_inputs = n_inputs
+        self.n_outputs = n_outputs
+        self.inputs = []
+        self.outputs = []
+        self.box_id = box_id
+        self.status = 'INIT'
+        self.json = json
+
+    def isRunned(self):
+        return self.status == 'RUNNED'    
+
+    def getStatus(self):
+        return self.status
+ 
+    def setStatus(self, status):
+        self.status = status
+        return self.status
+
+    def run(self):
+        print("Start running", self.box_id, self.isRunned())
+        try:
+            if self.isRunned():
+                return True
+       
+            self.setStatus('RUNNING')
+
+            myinputs = []
+
+            for i_input in self.inputs:
+                i_input.parentBox.run()
+                print("NUM TOTAL OUTS", len(i_input.parentBox.outputs))
+                print("BUSCANDO INPUT PORT", i_input.numport)
+                myinputs.append(i_input.parentBox.outputs[i_input.numport-i_input.parentBox.n_inputs])
+
+            out = c_run_str_code(myinputs, self.str_code)
+
+            for p_out in out:
+                print("añadiendo out", p_out)
+                self.outputs.append(p_out)
+        # after run 
+            index = 0
+            for out in self.outputs:
+                self.json['nodes'][self.box_id]['properties']['payload']['result']['out'+str(index)]['status'] = 'OK'
+                self.json['nodes'][self.box_id]['properties']['payload']['result']['out'+str(index)]['first100'] = out.head(100).to_json()
+                self.json['nodes'][self.box_id]['properties']['payload']['result']['out'+str(index)]['columns'] = out.columns.to_json()
+
+
+                index = index + 1
+            self.setStatus('RUNNED')
+        except Exception as e:
+            self.json['nodes'][self.box_id]['properties']['payload']['result']['error_message'] = e.message
+            self.json['nodes'][self.box_id]['properties']['payload']['result']['error_args'] = e.args
+            self.json['nodes'][self.box_id]['properties']['payload']['result']['error_trace'] = traceback.print_exc()
+            raise Exception('Run error check error message results!')
+            return False
+        print("End running", self.box_id, self.isRunned())
+        return self.isRunned()
+
+
+def run_celery_project(allboxes, project_id):
+
+    # REPLACE THIS QUERY FOR NEW ENDPOINT TO BACKEND TO RETRIVE ALL JSON DATA FROM PROJECT
+    project = Project.query.filter(Project.id == project_id).first()
+    print("START CELERY TASK")
+    allboxes = []
+    if project:
+        json_wf = project.json
+        d_json = json.loads(json_wf)
+        print(type(d_json))
+        print(d_json)
+        # allboxes = []
+        #for x in d_json['nodes']:
+        #    print("AAAA", x)
+        for x in d_json['nodes']:
+#            print("milog",d_json['nodes'][0]['type'])
+            box_type = d_json['nodes'][x]['type']
+            if (box_type[0:7] == 'Dataset'):
+                # node_name = d_json['nodes'][x]['id']
+                node_name = x
+                # TODO OK for now special case for dataset!i
+                from sklearn.datasets import load_boston
+                from sklearn.model_selection import cross_val_score
+                from sklearn.tree import DecisionTreeRegressor
+                boston = load_boston()
+
+                # regressor = DecisionTreeRegressor(random_state=0)
+                # cross_val_score(regressor, boston.data, boston.target, cv=10)                  
+                # import numpy as np
+                # from sklearn.linear_model import LinearRegression
+                # X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+                box = BoxCode("", node_name, 0, 1, d_json)
+                box.setStatus('RUNNED')
+                box.outputs.append(boston)
+                # for now dummy data on dataset
+                allboxes.append(box)
+
+            elif (box_type == ('Python Script')):
+                # TODO añadir box_type categoria sub categoria etc...
+                # se podrian llamar igual pero pertencer a otra categria
+                # no es importante pero si se pued mejor...
+                # O ES UN ENDPOINT
+                # node_name = d_json['nodes'][x]['id']
+                node_name = x
+                python_code = d_json['nodes'][x]['properties']['payload']['python_code']
+                n_inputs = d_json['nodes'][x]['properties']['payload']['n_input_ports']
+                n_outputs = d_json['nodes'][x]['properties']['payload']['n_output_ports']
+                box = BoxCode(python_code, node_name, n_inputs, n_outputs, d_json)
+                allboxes.append(box)
+
+        # TODO do it better
+        def getboxby_name(box_name):
+            for x in allboxes:
+                if x.box_id == box_name:
+                    return x
+            return None
+        # TODO do it better
+        def getportid_to_index(portid):
+            return portid.split('port')[1]
+
+        for x in d_json['links']:
+            orig_box_id = d_json['links'][x]['from']['nodeId']
+            orig_input_port = d_json['links'][x]['from']['portId']
+            
+            dest_box_id = d_json['links'][x]['to']['nodeId']
+            dest_input_port = d_json['links'][x]['to']['portId']
+
+            # need locate orig_box
+            orig_box = getboxby_name(orig_box_id)
+            orig_id = getportid_to_index(orig_input_port)
+            
+            # need locate dest_box
+            dest_box = getboxby_name(dest_box_id)
+            dest_id = getportid_to_index(dest_input_port)
+            if dest_box.box_id != orig_box.box_id:
+                input_port = InputPort(orig_input_port, int(orig_id)-1, orig_box)
+                dest_box.inputs.append(input_port)
+
+
+        # now select one untrained box and train until allboxes trained
+        # TODO IN random way
+        pendingTrain = True
+        while pendingTrain:
+            pendingTrain=False
+            for x in allboxes:
+                if x.isRunned()==False:
+                    x.run()
+                    pendingTrain=True
+
+        # print("BOX_ID-->",box_id)
+    return allboxes
+
+def get_key_from_redis():
+
+    keyId = '1212'
+
+    ret = KeyId
+    return None
+
+def run_str_code(func):
+    global ret
+    ret = []
+    LOC1 = """def """+func+"""(num):"""
+    LOC = """
+    num*2 
+    print("hola")
+    return num
+"""
+    exec(LOC1+LOC)
+  
+    return ret
+
+
+if __name__ == 'main':
+    allproject = None
+    project_id = sys.argv[1]
+    while True
+        task=get_key_from_redis(project_id)
+        if task:
+            allproject = run_celery_project(allproject, project_id, task)
+
+        time.sleep(1)
+
